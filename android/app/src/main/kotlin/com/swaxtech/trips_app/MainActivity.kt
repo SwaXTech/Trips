6963ced9b4d1bdbfae4f4f30772d02e7c721dae4
SwaXTech <aleeherasimiuk@gmail.com>
2020-09-04 18:20:58 -0300

    Appbar for android is now transparent

--- android/app/src/main/kotlin/com/swaxtech/trips_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/swaxtech/trips_app/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.swaxtech.trips_app
 
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        this.flutterEngine?.let { GeneratedPluginRegistrant.registerWith(it) }
+        this.window.statusBarColor = android.graphics.Color.TRANSPARENT;
+    }
+
+
 }
