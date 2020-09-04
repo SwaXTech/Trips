b046a8c2b2a1a0defe73b4e1a3d8763617ff55d3
SwaXTech <aleeherasimiuk@gmail.com>
2020-09-04 18:08:06 -0300

    Added gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'description_places.dart';
 import 'review_list.dart';
+import 'gradient.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,11 +18,18 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Trips App"),
-        ),
-        //body: new DescriptionPlace("Bahamas", 4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eu lectus vitae auctor. Nunc rhoncus, leo nec malesuada consectetur, lorem est convallis orci, non pulvinar felis magna vitae arcu. Nulla suscipit consequat lectus, eu efficitur ante egestas nec. Morbi ipsum erat, tempor vel est eget, blandit blandit sem. Duis pharetra nunc dui, at finibus augue porttitor sit amet. Curabitur nec ultrices metus. Curabitur id lorem vestibulum, posuere justo in, sagittis sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin interdum tincidunt justo, non aliquam orci bibendum a. Sed placerat varius mollis. Duis sed iaculis tellus, et volutpat enim. Suspendisse placerat odio sem, vitae efficitur orci tincidunt sed."),
-        body: ReviewList()
+        //body: new DescriptionPlace(Proin interdum tincidunt justo, non aliquam orci bibendum a. Sed placerat varius mollis. Duis sed iaculis tellus, et volutpat enim. Suspendisse placerat odio sem, vitae efficitur orci tincidunt sed."),
+        body: Stack(
+          children: [
+            ListView(
+              children: [
+                DescriptionPlace("Bahamas", 4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit eu lectus vitae auctor. Nunc rhoncus, leo nec malesuada consectetur, lorem est convallis orci, non pulvinar felis magna vitae arcu. Nulla suscipit consequat lectus, eu efficitur ante egestas nec. Morbi ipsum erat, tempor vel est eget, blandit blandit sem. Duis pharetra nunc dui, at finibus augue porttitor sit amet. Curabitur nec ultrices metus. Curabitur id lorem vestibulum, posuere justo in, sagittis sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. "),
+                ReviewList()
+              ],
+            ),
+            GradientBack("Popular"),
+          ],
+        )
       ),
     );
   }
