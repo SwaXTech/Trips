e79fa2b785b7cef3b717f0b08a32a5a38c390313
SwaXTech <aleeherasimiuk@gmail.com>
2020-09-08 18:49:02 -0300

    Added infocard into placecard

---
+++ lib/place_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:trips_app/floating_action_button_green.dart';
+
+class PlaceCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 15, right: 15),
+          height: 260,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            shape: BoxShape.rectangle,
+            image: DecorationImage(
+                fit: BoxFit.cover, //Siempre al centro
+                image: AssetImage('assets/img/mountain.jpg')),
+          ),
+        ),
+        Container(
+          height: 120,
+          width: 300,
+          margin: EdgeInsets.only(top: 270),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(22)),
+              color: Colors.white,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.black38,
+                    blurRadius: 15.0,
+                    offset: Offset(0.0, 7.0))
+              ]),
+          child: Stack(
+            alignment: Alignment(0.9, 1.1),
+            children: [
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Knuckles Mountains Range",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w900,
+                        fontFamily: 'Lato'
+                      ),
+                    ),
+                    Spacer(),
+
+                    Text("Hiking, Water fall hunting, Natural bath, Scenery & Photograpy",
+                    style: TextStyle(
+                      color: Colors.black26,
+                      fontFamily: 'Lato',
+                      fontSize: 12.0,
+                    ),
+                    ),
+                    Spacer(),
+                    Text("Steps 123,123,123",
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                    ),),
+
+                  ],
+                ),
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+              ),
+              FloatingActionButtonGreen(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trips_app/place_card.dart';
 import 'package:trips_app/profile_info.dart';
 
 import 'buttons_row.dart';
@@ -12,6 +13,10 @@ class ProfileTrips extends StatelessWidget{
         GradientBack("Perfil", 460.0),
         ProfileInfo("Pathum Tzoo", "pathumtzoo1@gmail.com", 'assets/img/a_person.jfif'),
         ButtonsRow(),
+        Container(
+          margin: EdgeInsets.only(top: 270),
+          child: PlaceCard(),
+        ),
       ],
     );
   }
