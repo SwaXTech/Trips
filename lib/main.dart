b54658f2c62377688b09fd832cb2576f78a004a0
SwaXTech <aleeherasimiuk@gmail.com>
2020-09-08 22:36:48 -0300

    Using providers. Creating classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:trips_app/User/bloc/bloc_user.dart';
 import 'package:trips_app/trips.dart';
 import 'package:trips_app/trips_cupertino.dart';
 import 'Place/ui/widgets/description_places.dart';
@@ -13,13 +15,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Trips App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider(
+      bloc: UserBloc(),
+      child: MaterialApp(
+        title: 'Trips App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Trips(),
       ),
-      home: Trips(),
     );
   }
 }
